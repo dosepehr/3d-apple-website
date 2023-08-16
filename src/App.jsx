742b12af77a50cc64549f15988a756f37f79187f
dosepehr <dosepehr@gmail.com>
2023-08-16 15:50:08 +0330

    add SoundSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Jumborton from './components/Jumbotron';
+import SoundSection from './components/SoundSection';
 
 const App = () => {
     return (
         <>
             <div className='App'>
-          <Nav />
-          <Jumborton/>
+                <Nav />
+                <Jumborton />
+                <SoundSection />
             </div>
         </>
     );
---
+++ src/components/SoundSection.jsx
@@ -0,0 +1,25 @@
+const SoundSection = () => {
+    return (
+        <>
+            <div className='sound-section wrapper'>
+                <div className='body'>
+                    <div className='sound-section-content content'>
+                        <h2 className='title'>New Sound System</h2>
+                        <p className='text'>Teel the base</p>
+                        <span className='description'>
+                            From $41.62/mo. for 24 mo. or $999 before trade-in
+                        </span>
+                        <ul className='links'>
+                            <li className='button'>Buy</li>
+                            <li>
+                                <a href='' className='link'>Learn more</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SoundSection;
